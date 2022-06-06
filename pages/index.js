f4eb3406a85df0a6f95ba8ea3160c7b6d0a795fd
davadeola <adeoladavid15@gmail.com>
2022-06-06 19:18:47 +0300

    created custom components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Food Shoop Online</h2>
+        <p>Something for everyone</p>
+      </div>
+
+      <div className="products-container"></div>
+
+      <FooterBanner />
+    </div>
+  );
 };
 
 export default Home;
