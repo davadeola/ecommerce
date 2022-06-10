914420124191fa9e76f692afa4059bf09d160920
davadeola <adeoladavid15@gmail.com>
2022-06-10 13:19:50 +0300

    created the success page

--- context/StateContext.js
+++ context/StateContext.js
@@ -140,6 +140,9 @@ export const StateContext = ({ children }) => {
         onAdd,
         toggleCartItemQuantity,
         onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
